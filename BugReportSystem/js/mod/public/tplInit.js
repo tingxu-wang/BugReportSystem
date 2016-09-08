6d2967a02b8fdf07b39fce9cd9049a04c026ee99
wangtingxu <wangtingxu@enhance.cn>
2016-09-08 12:02:11 +0800

    模板功能实现

--- BugReportSystem/js/mod/public/tplInit.js
+++ BugReportSystem/js/mod/public/tplInit.js
@@ -1,10 +1,14 @@
 /*
  * create by wtx 20160908
- * 全局模板初始化，用于根据不同的用户类别展示相应的侧边栏内容
+ * 全局模板初始化，用于根据不同的用户类别展示相应的侧边栏内容,用户名显示
 */
 
 define(function (require,exports,module){
 	var sidebarTpl=require('../../../tpl/sidebar.tpl')
+	var render=template.compile(sidebarTpl)
 
+	$('.page-sidebar-menu').html(render({code:$.cookie('code')}))
+
+	$('.cus-user .username').html($.cookie('name'))
 	
 })
