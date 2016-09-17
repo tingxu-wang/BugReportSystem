d36478212ebdbefb543eb12aa853e7b2f115e77d
wangtingxu <wangtingxu@enhance.cn>
2016-09-17 16:59:58 +0800

    debug

--- BugReportSystem/js/mod/public/prjTable.js
+++ BugReportSystem/js/mod/public/prjTable.js
@@ -9,6 +9,7 @@ define(function (require, epxorts, module){
 		$.cookie('detail_pid',pid)//点击的项目id保存到cookie中
 
 		console.log(pid)
+		
 		//window.location.href='prjdetail.html'		
 	})
 /*	$('.js-prj-table tr').on('click',function(){
